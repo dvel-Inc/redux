265ec7497be54b57ad5ee9ed983592165b57baae
phips28 <phips28@gmx.at>
2018-09-18 21:36:02 +0200

    remove errors when isDispatching

--- src/createStore.js
+++ src/createStore.js
@@ -64,14 +64,6 @@ export default function createStore(reducer, preloadedState, enhancer) {
    * @returns {any} The current state tree of your application.
    */
   function getState() {
-    if (isDispatching) {
-      throw new Error(
-        'You may not call store.getState() while the reducer is executing. ' +
-          'The reducer has already received the state as an argument. ' +
-          'Pass it down from the top reducer instead of reading it from the store.'
-      )
-    }
-
     return currentState
   }
 
@@ -103,15 +95,6 @@ export default function createStore(reducer, preloadedState, enhancer) {
       throw new Error('Expected the listener to be a function.')
     }
 
-    if (isDispatching) {
-      throw new Error(
-        'You may not call store.subscribe() while the reducer is executing. ' +
-          'If you would like to be notified after the store has been updated, subscribe from a ' +
-          'component and invoke store.getState() in the callback to access the latest state. ' +
-          'See https://redux.js.org/api-reference/store#subscribe(listener) for more details.'
-      )
-    }
-
     let isSubscribed = true
 
     ensureCanMutateNextListeners()
@@ -120,13 +103,6 @@ export default function createStore(reducer, preloadedState, enhancer) {
     return function unsubscribe() {
       if (!isSubscribed) {
         return
-      }
-
-      if (isDispatching) {
-        throw new Error(
-          'You may not unsubscribe from a store listener while the reducer is executing. ' +
-            'See https://redux.js.org/api-reference/store#subscribe(listener) for more details.'
-        )
       }
 
       isSubscribed = false
